17cbad0ba2d6601f887d344cf0e517b14195c51d
grushanina <61738785+grushanina@users.noreply.github.com>
2020-05-26 16:15:05 +0300

    Add files via upload

---
+++ hash/hash/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace hash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea7.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea7);
+            legend7.Name = "Legend1";
+            this.chart1.Legends.Add(legend7);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series7.ChartArea = "ChartArea1";
+            series7.Legend = "Legend1";
+            series7.Name = "Series1";
+            this.chart1.Series.Add(series7);
+            this.chart1.Size = new System.Drawing.Size(399, 233);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(426, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(427, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 20);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Make hash";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(507, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(507, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(426, 73);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(181, 87);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 251);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 277);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox5.Size = new System.Drawing.Size(595, 161);
+            this.textBox5.TabIndex = 7;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(119, 251);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 20);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Find";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "c";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ hash/hash/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace hash
+{
+    public partial class Form1 : Form
+    {
+        public int n;
+        public string path;
+        public List<string>[] rows_public;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int f(int number, int size)
+        {
+            return (number%size);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            path = openFileDialog1.FileName;
+            StreamReader sr1 = new StreamReader(path, Encoding.GetEncoding(1251));
+
+            n = 0;
+            while ((sr1.ReadLine()) != null)
+                n++;
+
+
+        
+
+
+            textBox1.Text = n.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            chart1.Series["Series1"].Points.Clear();
+            textBox3.Clear();
+            int x = 0;
+            if (textBox2.Text == null || !Int32.TryParse(textBox2.Text, out x))
+            {
+                return;
+            }
+
+
+
+            List<string>[] rows = new List<string>[Int32.Parse(textBox2.Text)];
+            for (int i = 0; i < rows.Length; i++)
+                rows[i] = new List<string>();
+
+            StreamReader sr2 = new StreamReader(path, Encoding.GetEncoding(1251));
+            string line;
+            while ((line = sr2.ReadLine()) != null)
+            {
+                string word = "";
+
+                for (int j = 0; j < line.Length; j++)
+                {
+                    if (line[j] != ' ')
+                    {
+                        word += line[j];
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+                rows[f(Int32.Parse(word), rows.Length)].Add(line);
+            }
+
+            rows_public = rows;
+
+            int[] counts = new int[rows.Length];
+
+            for (int i = 0; i < rows.Length; i++)
+            {
+                
+                chart1.Series["Series1"].Points.AddXY(i, rows[i].Count);
+                counts[i] = rows[i].Count;
+            }
+
+            textBox3.Text += "Max = " + counts.Max() + "\r\n" + "Min = " + counts.Min() + "\r\n" + "Avg = " + counts.Average() + "\r\n";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox5.Clear();
+            int x = 0;
+            if (textBox4.Text == null || !Int32.TryParse(textBox4.Text, out x))
+            {
+                return;
+            }
+
+            string line;
+
+            for (int i = 0; i < rows_public[f(Int32.Parse(textBox4.Text), rows_public.Length)].Count; i++)
+            {
+                if (rows_public[f(Int32.Parse(textBox4.Text), rows_public.Length)][i].Substring(0, 5) == textBox4.Text)
+                {
+                    textBox5.Text += rows_public[f(Int32.Parse(textBox4.Text), rows_public.Length)][i] + "\r\n";
+                }
+            }
+
+            textBox5.Text += "В ячейке:" + "\r\n";
+
+            for (int i = 0; i < rows_public[f(Int32.Parse(textBox4.Text), rows_public.Length)].Count; i++)
+            {
+
+                textBox5.Text += rows_public[f(Int32.Parse(textBox4.Text), rows_public.Length)][i] + "\r\n";
+            }
+
+            //textBox5.Text += rows_public.[f(Int32.Parse(textBox4.Text), rows_public.Length)];
+        }
+    }
+}
